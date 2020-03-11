dd61913c8f5822e6d03c2f14c1188e74a90c3c30
romipraveen1 <romipraveen100@gmail.com>
2020-03-11 10:10:20 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/tokyo-supermix-front-end/precache-manifest.c5ac5a0546493272c859ff03440cfd28.js"
+  "/tokyo-supermix-front-end/precache-manifest.58fe73dba7edae862ffe66b76183b3b2.js"
 );
 
 self.addEventListener('message', (event) => {
